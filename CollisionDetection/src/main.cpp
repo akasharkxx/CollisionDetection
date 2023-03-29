8b9efd8adb9afd3a9d083ef350d2d9428adc420d
R1pHunt3r <akash.jaiswar41@gmail.com>
2023-03-29 21:47:15 +0530

    Update main.cpp

--- CollisionDetection/src/main.cpp
+++ CollisionDetection/src/main.cpp
@@ -35,19 +35,71 @@ bool RectRectCollision(Rectangle A, Rectangle B)
 // TODO : Add buffer to remove jerkyness in collision
 bool LinePointCollision(Line A, Point B)
 {
-	return (distance(A.PointOne, B) + distance(A.PointTwo, B)) == distance(A.PointOne, A.PointTwo);
+	float epsilon = 0.0001f;
+	return std::abs((distance(A.PointOne, B) + distance(A.PointTwo, B)) - distance(A.PointOne, A.PointTwo)) < epsilon;
+}
+
+bool LineCircleCollision(Line A, Circle B)
+{
+	bool CollidesPoint1 = CirclePointCollision(B, A.PointOne);
+	bool CollidesPoint2 = CirclePointCollision(B, A.PointTwo);
+
+	if (CollidesPoint1 || CollidesPoint2)
+	{
+		return true;
+	}
+
+	int x1 = A.PointOne.x;
+	int x2 = A.PointTwo.x;
+	int xk = B.center.x;
+	int y1 = A.PointOne.y;
+	int y2 = A.PointTwo.y;
+	int yk = B.center.y;
+
+	float u = ((xk - x1) * (x2 - x1) + (yk - y1) * (y2 - y1)) / pow((distance(A.PointOne, A.PointTwo)), 2);
+
+	float x = x1 + u * (x2 - x1);
+	float y = y1 + u * (y2 - y1);
+
+	Point P;
+	P.x = x;
+	P.y = y;
+
+	if (!LinePointCollision(A, P))
+	{
+		return false;
+	}
+	else
+	{
+		return CirclePointCollision(B, P);
+	}
+}
+
+bool PointRectangelCollision(Point P, Rectangle R)
+{
+	if (P.x >= R.corner.x && P.x <= R.corner.x + R.width && P.y >= R.corner.y && P.y <= R.corner.y + R.height)
+	{
+		return true;
+	}
+
+	return false;
 }
 
 int main()
 {
 	Point A(5, 4);
 	Point B(4, 5);
-	Circle C(A, 5);
+	Point A1(-4, -5);
+	Circle C(A, 2);
+	Circle C1(A1, 2);
 	Circle D(B, 3);
+	Line L(A, B);
 
 	std::cout << PointCollision(A, B) << std::endl;
 	std::cout << CirclePointCollision(C, B) << std::endl;
 	std::cout << CircleCircleCollision(C, D) << std::endl;
+	std::cout << LineCircleCollision(L, C) << std::endl;
+	std::cout << LineCircleCollision(L, C1) << std::endl;
 
 	std::cin.get();
 }
